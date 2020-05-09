fdd8c66a1b64c889df3cbc7abd718e513c78f607
Marius <Marius.Vasili@fti.edu.al>
2020-05-09 17:30:38 +0200

    P

--- client/src/pages/TourDetails/TourDetails.js
+++ client/src/pages/TourDetails/TourDetails.js
@@ -16,7 +16,7 @@ import ReviewStatistics from './ReviewStatistics';
 import Reviews from './Reviews';
 import AgencyInfo from './AgencyInfo';
 // import Logo from '../../assets/logo.png';
-import logo from '../../assets/logo.png';
+// import logo from '../../assets/logo.png';
 import Button from '../../shared/components/Button/Button';
 import { addToCart } from '../../store/actions/userActions';
 import { loadStripe } from '@stripe/stripe-js';
@@ -483,7 +483,7 @@ const TourDetails = React.memo((props) => {
       <div className="bookTour__container">
         <div className="bookTour__info">
           <div className="bookTour__images">
-            <img className="bookTour__image" src={logo} />
+            {/* <img className="bookTour__image" src={logo} /> */}
             {tour.images.map((img) => (
               <img
                 className="bookTour__image"
